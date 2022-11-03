c90c3592c6437f7adde982b1719b0fb5045400fe
sheldonwafula <wafulasheldon2@gmail.com>
2022-11-03 16:49:00 +0300

    adding skills section

---
+++ index2.js
@@ -0,0 +1,8 @@
+let open=document.querySelector('.menu');
+
+
+open.addEventListner('click',openup)
+function openup(){
+    let list=document.querySelector('.list-1');
+    list.style.display="block"
+}
